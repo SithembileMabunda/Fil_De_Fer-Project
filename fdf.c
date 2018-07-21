effc124d14bede175bce03e966b390bb62f877b2
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2018-07-21 13:04:21 +0200

    Update fdf.c

--- fdf.c
+++ fdf.c
@@ -18,19 +18,9 @@ int		main(void)
 {
 	void *window;
 	void *mlx;
-	int x;
-	int y;
-
-	x = 300;
-	y = 600;
+	
 	mlx = mlx_init();
 	window = mlx_new_window(mlx, 1000, 1000, "practice");
-	while ((x >= 250 && x <= 750) || (y >= 250 && y <= 750))
-	{
-		mlx_pixel_put(mlx, window, x, y, 0x00FFFFFF);
-		x++;
-		y++;
-	}
 	mlx_key_hook(window, ft_exit_esc_quit, 0);
 	mlx_loop(mlx);
 }
