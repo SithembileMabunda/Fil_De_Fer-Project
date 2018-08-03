f9e36ef799e90cf86e0034d479eeacc5e70526c1
Sithembile <39857574+SithembileMabunda@users.noreply.github.com>
2018-08-03 13:33:42 +0200

    Update fdf.h

--- fdf.h
+++ fdf.h
@@ -15,9 +15,29 @@
 
 typedef	struct	s_axis
 {
-	int			x;
-	int			y;
-	int			z;
-}				t_axis;
+	int	x;
+	int	y;
+	int	z;
+}		t_axis;
+
+typedef	struct	s_map
+{
+	int		width;
+	int		height;
+	t_vector	**vector;
+}		t_map;
+
+typedef	struct	s_vector
+{
+	double		vert;
+	double		horz;
+	double		diag;
+}		t_vector;
+
+typedef		s_
+{
+	int		lat;
+	int		lng;
+}		t_;
 
 #endif
